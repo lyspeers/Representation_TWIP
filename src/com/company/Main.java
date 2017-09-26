2f6f7d149cecef053b895212a3b9d925d109e1f2
ls059 <lyspeers@gmail.cim>
2017-09-26 09:01:18 -0400

    1.0.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,31 +6,39 @@ public class Main {
     /*String hi = "hi";     Bianary, octal, decimal, hexidecimal, ASCII
         System.out.print(Integer.toHexString((int) hi.charAt(1)));*/
         //need scanner
+        String a  ="",b = "",c = "",d = "",e  = "";
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter a word to be encoded");
         String x = input.nextLine();
-
-        String z = "Hello";
         int encoder = (int)(Math.random()*x.length());
+
+
+
         for(int i = 0;i < x.length();i++){
-            if(encoder%5 == 0){
+            if(encoder % 5 == 0){
+                 a = Integer.toBinaryString(((int) x.charAt(i)));
                 System.out.print(Integer.toBinaryString(((int) x.charAt(i))));
                 encoder++;
-            }else if(encoder%5 == 1){
+            }else if(encoder % 5 == 1){
+                 b = Integer.toOctalString((int) x.charAt(i));
                 System.out.print(Integer.toOctalString((int) x.charAt(i)));
                 encoder++;
-            }else if(encoder%5 == 2){
+            }else if(encoder % 5 == 2){
                 char y = x.charAt(i);
+                 c = "" +(int) y;
                 System.out.print((int) y);
                 encoder++;
-            }else if(encoder%5 == 3){
+            }else if(encoder % 5 == 3){
+                 d = Integer.toHexString((int) x.charAt(i));
                 System.out.print(Integer.toHexString((int) x.charAt(i)));
                 encoder++;
             }else {
+                 e = ""  + x.charAt(i);
                System.out.print(x.charAt(i));
                 encoder++;
 
             }
-        }
+        }//String encoded = a + b + c + d + e; This doesn't work for strings longer that 5 letters
+
     }
 }
